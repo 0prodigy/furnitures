f89ee615693acb08793aee60ef9c51d27a2738f9
Akash <pathakvikash9211@gmail.com>
2020-08-13 02:25:11 +0530

    Create product component

--- src/components/client/Home.jsx
+++ src/components/client/Home.jsx
@@ -2,17 +2,55 @@ import React from "react";
 import Sidebar from "./Sidebar";
 import Banner from "./Banner";
 import styled from "styled-components";
+import Product from "./Product";
 
 const Wrapper = styled.div`
   display: flex;
 `;
+const Products = styled.div`
+  display: flex;
+  flex-wrap: warp;
+  padding: 20px;
+  max-width: 1100px;
+  margin: auto;
+  > div {
+    flex: 33.33%;
+    max-width: 33.33%;
+  }
+`;
+
+const Title = styled.div`
+  padding: 20px 35px 5px;
+  max-width: 1100px;
+  margin: auto;
+  text-align: center;
+  h1 {
+    font-size: 2.4rem;
+    font-weight: 400;
+    color: #333;
+  }
+`;
 
 function Home() {
   return (
-    <Wrapper>
-      <Sidebar></Sidebar>
-      <Banner />
-    </Wrapper>
+    <>
+      <Wrapper>
+        <Sidebar></Sidebar>
+        <Banner />
+      </Wrapper>
+      <Title>
+        <h1>Chairs</h1>
+        <p>We provide world best wooden chair all over the world</p>
+      </Title>
+      <Products>
+        <Product
+          name="Wooden Chair"
+          type="Chair"
+          img="img/greenchair.PNG"
+          price="343"
+        />
+      </Products>
+    </>
   );
 }
 
---
+++ src/components/client/Product.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  .img-container {
+    overflow: hidden;
+    img {
+      width: 100%;
+    }
+    &:hover img {
+      transition: all 1s ease;
+      transform: scale(1.2);
+    }
+  }
+  .details {
+    padding: 20px 5px;
+    .left {
+      float: left;
+    }
+    h5 {
+      font-size: 1.2rem;
+      color: #333;
+    }
+    p {
+      color: #3e3e3e;
+    }
+    .right {
+      float: right;
+    }
+  }
+  .clear {
+    clear: both;
+  }
+`;
+
+function Product(props) {
+  const { name, img, type, price } = props;
+  return (
+    <Wrapper>
+      <div>
+        <div className="img-container">
+          <img src={img} alt={name} />
+        </div>
+        <div className="details">
+          <div className="left">
+            <h5>{name}</h5>
+            <p>{type}</p>
+          </div>
+          <div className="right">
+            <p>{price}</p>
+          </div>
+          <div className="clear"></div>
+        </div>
+      </div>
+    </Wrapper>
+  );
+}
+
+export default Product;
