9c5195af8960a8b973214c7b8b171cebc13924b1
Akash <pathakvikash9211@gmail.com>
2020-08-12 23:55:49 +0530

    Created sidebar component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import "./App.css";
+import Home from "./components/client/Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/client/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Sidebar from "./Sidebar";
+
+function Home() {
+  return (
+    <div>
+      <Sidebar></Sidebar>
+    </div>
+  );
+}
+
+export default Home;
